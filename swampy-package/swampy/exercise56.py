2762bf235566f6e23f1d19b93d16fa91d39f9560
Amaka Atoyebi <amaka.atoyebi@gmail.com>
2013-08-08 16:33:49 -0400

    update to koch curve problem

--- swampy-package/swampy/exercise56.py
+++ swampy-package/swampy/exercise56.py
@@ -4,29 +4,58 @@ bob = Turtle()
 bob.delay = .01		
 
 def koch(t, length, angle):
-	'''if n == 0:
-		return'''
 	fd(t, length/3)
 	lt(t, angle)
 	fd(t, length/3)
+
+
+def transition(t, length, angle):
+	bk(t, length/3)
 	rt(t, 2*angle)
 	fd(t, length/3)
 	lt(t, angle)
-	fd(t, length/3)	
 
 def snowflake(t, length, angle):
-	koch(t, length, angle)
-	rt(t, angle*2)
-	koch(t, length, angle)
-	rt(t, angle*2)
-	koch(t, length, angle)
-	rt(t, angle*2)
-	koch(t, length, angle)
-	rt(t, angle*2)
-	koch(t, length, angle)
-	rt(t, angle*2)
-	koch(t, length, angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		transition(t, length, angle)
+		transition(t, length, angle)
+		transition(t, length, angle)
+		transition(t, length, angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		transition(t, length, angle)
+		transition(t, length, angle)
+		transition(t, length, angle)
+		transition(t, length, angle)
+		rt(t, 6*angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		koch(t, length, angle)
+		rt(t, 2*angle)
+		transition(t, length, angle)
+		transition(t, length, angle)
+		transition(t, length, angle)
+		transition(t, length, angle)
+		koch(t, length, angle)
 
-snowflake(bob, 90, 60)		
+
+
+snowflake(bob, 90, 60)
+
 
 wait_for_user()
