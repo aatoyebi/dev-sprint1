9872ff9fec45c8dde3c7700805b28e6b3d8161df
Amaka Atoyebi <amaka.atoyebi@gmail.com>
2013-08-13 11:47:00 -0400

    Koch curve update

--- swampy-package/swampy/exercise56.py
+++ swampy-package/swampy/exercise56.py
@@ -3,21 +3,31 @@ world = TurtleWorld()
 bob = Turtle()				
 bob.delay = .01		
 
-def koch(t, length, angle):
-	fd(t, length/3)
-	lt(t, angle)
-	fd(t, length/3)
+def koch(t, length):
+	if (length<3):
+		fd(t, length)
+		return
+	koch(t, length/3)
+	lt(t, 60)
+	koch(t, length/3)
+	rt(t, 120)
+	koch(t, length/3)
+	lt(t, 60)
+	koch(t, length/3)
 
 
-def transition(t, length, angle):
+'''def transition(t, length, angle):
 	for i in range(4):
 		bk(t, length/3)
 		rt(t, 2*angle)
 		fd(t, length/3)
-		lt(t, angle)
+		lt(t, angle)'''
 
-def snowflake(t, length, angle):
-		koch(t, length, angle)
+def snowflake(t, length):
+	for i in range(3):
+		koch(t, length)
+		rt(t, 120)
+		'''koch(t, length, angle)
 		rt(t, 2*angle)
 		koch(t, length, angle)
 		rt(t, 2*angle)
@@ -43,11 +53,11 @@ def snowflake(t, length, angle):
 		koch(t, length, angle)
 		rt(t, 2*angle)
 		transition(t, length, angle)
-		koch(t, length, angle)
+		koch(t, length, angle)'''
 
 
 
-snowflake(bob, 90, 60)
+snowflake(bob, 300)
 
 
 wait_for_user()
